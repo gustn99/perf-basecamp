82efe41eaa9b40089fc7fcff1d3eaa89d774f93b
gustn99 <azerosong@gmail.com>
2026-09-16 23:17:38 +0900

    Updates

--- bundle.js
+++ bundle.js
@@ -10470,7 +10470,7 @@ var gifAPIService_awaiter = (undefined && undefined.__awaiter) || function (this
     });
 };
 
-const API_KEY = "MISSING_ENV_VAR".GIPHY_API_KEY;
+const API_KEY = "lWUTQhiPBMPgiCuQVuuETX7NEM1OVqTA";
 if (!API_KEY) {
     throw new Error('GIPHY_API_KEY is not set in environment variables');
 }
